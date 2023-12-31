2661fa39ef411632af6d4aeb3811d1e5c05ed7da
tylerreed <tylerreedytlearning@gmail.com>
2023-12-30 21:45:11 -0500

    added a few examples of directly connecting to LM Studio to use open-source LLMs with Python directly

---
+++ ai_agency_02_lmstudio/example_02.py
@@ -0,0 +1,31 @@
+# Chat with an intelligent assistant in your terminal
+from openai import OpenAI
+
+client = OpenAI(base_url="http://localhost:1234/v1", api_key="not-needed")
+
+history = [
+    {"role": "system",
+     "content": "You are an intelligent assistant. You always provide well-reasoned answers that are both correct and "
+                "helpful."},
+    {"role": "user",
+     "content": "Hello, introduce yourself to someone opening this program for the first time. Be concise."},
+]
+
+while True:
+    completion = client.chat.completions.create(
+        model="local-model", #doesn't matter...
+        messages=history,
+        temperature=0.7,
+        stream=True,
+    )
+
+    new_message = {"role": "assistant", "content": ""}
+
+    for chunk in completion:
+        if chunk.choices[0].delta.content:
+            print(chunk.choices[0].delta.content, end="", flush=True)
+            new_message["content"] += chunk.choices[0].delta.content
+
+    history.append(f"new message: {new_message}")
+    print()
+    history.append({"role": "user", "content": input("> ")})
