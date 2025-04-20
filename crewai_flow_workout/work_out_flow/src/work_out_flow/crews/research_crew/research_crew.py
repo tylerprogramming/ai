a822f464972c06acc854807dfa5120f199d762a3
tylerreed <tylerreedytlearning@gmail.com>
2025-04-19 22:50:52 -0400

    heavily updated this flow to show what can be done with an AI automation with multiple crews

---
+++ crewai_flow_workout/work_out_flow/src/work_out_flow/crews/research_crew/research_crew.py
@@ -0,0 +1,46 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from composio_crewai import ComposioToolSet, Action, App
+from pydantic import BaseModel
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+composio_toolset = ComposioToolSet(api_key="wsl1jgdjpr3s22exu1rkl")
+tools = composio_toolset.get_tools(actions=['FIRECRAWL_SEARCH', 'FIRECRAWL_CRAWL_URLS'])
+
+class WorkOutResearch(BaseModel):
+    research: str = ""
+    links: list[str] = []
+
+@CrewBase
+class ResearchCrew:
+    """Research Crew"""
+
+    agents_config = "config/agents.yaml"
+    tasks_config = "config/tasks.yaml"
+
+    @agent
+    def research_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config["research_agent"],
+        )
+
+    @task
+    def research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["research_task"],
+            output_pydantic=WorkOutResearch
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the Research Crew"""
+
+        return Crew(
+            agents=self.agents,  # Automatically created by the @agent decorator
+            tasks=self.tasks,  # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+        )
