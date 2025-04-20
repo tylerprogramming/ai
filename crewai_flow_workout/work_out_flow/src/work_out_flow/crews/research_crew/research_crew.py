d79aa91b23d63fef44d3b399e9fdeb6fdf1923f6
tylerreed <tylerreedytlearning@gmail.com>
2025-04-19 22:58:02 -0400

    updated research crew

--- crewai_flow_workout/work_out_flow/src/work_out_flow/crews/research_crew/research_crew.py
+++ crewai_flow_workout/work_out_flow/src/work_out_flow/crews/research_crew/research_crew.py
@@ -1,3 +1,5 @@
+import os
+
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from composio_crewai import ComposioToolSet, Action, App
@@ -7,7 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-composio_toolset = ComposioToolSet(api_key="wsl1jgdjpr3s22exu1rkl")
+composio_toolset = ComposioToolSet(api_key=os.getenv("COMPOSIO_API_KEY"))
 tools = composio_toolset.get_tools(actions=['FIRECRAWL_SEARCH', 'FIRECRAWL_CRAWL_URLS'])
 
 class WorkOutResearch(BaseModel):
